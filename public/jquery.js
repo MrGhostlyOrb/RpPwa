edea4b7448f361fbc2d45a24089ea066f726c0e5
Ben <bassbencooper999@gmail.com>
2020-05-03 22:06:45 +0100

    Fixed json parsing product file

--- public/jquery.js
+++ public/jquery.js
@@ -1,11 +1,27 @@
-let data = {"resultDescription":"SUCCESS","data":[{"orderNum":"A000","userName":"Oswaldo","value":504.74,"qty":3.0},{"orderNum":"A001","userName":"Mao","value":529.17,"qty":6.0},{"orderNum":"A002","userName":"Angeline","value":553.6,"qty":9.0},{"orderNum":"A003","userName":"Gerardo","value":578.03,"qty":12.0},{"orderNum":"A004","userName":"Nicki","value":602.46,"qty":15.0}]}
+$.getJSON("/products.json", function(json){
 
-$.each(data.data, function (index, item) {
-    var ul = $("<ul/>");
-    var li = $("<li/>", { text: item.orderNum });
-    ul.append(li);
-    ul.append(li.clone().text(item.qty));
-    ul.append(li.clone().text(item.userName));
-    ul.append(li.clone().text(item.value));
+
+console.log(json)
+
+
+$.each(json.data, function (index, item) {
+
+    //var ul = $("<ul/>");
+    //var li = $("<li/>", { text: item.orderNum });
+    //ul.append(li);
+    //ul.append(li.clone().text(item.qty));
+    //ul.append(li.clone().text(item.userName));
+    //ul.append(li.clone().text(item.value));
+    
+    var order = "<li>" + item.orderNum + "</li>";
+    var quantity = "<li>" + item.qty + "</li>";
+    var user = "<li>" + item.userName + "</li>";
+    var val = "<li>" + item.value + "</li>"
+    var button = "<button onclick = 'addJson(" + item.qty + "," + item.value + ")'>Click Me</button>"
+
+   	var ul = "<ul class = individualProductList>" + order + quantity + user + val + button + "</ul>";
+    
     $(".container").append(ul);
+});
+
 });
