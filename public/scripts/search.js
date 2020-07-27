5d1c1ddae873a776d1ab79cabbeece5946180b1e
Ben <bassbencooper999@gmail.com>
2020-07-27 19:37:56 +0100

    Added lazy loading for iframe

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,23 +1,24 @@
 console.log('Search loaded');
-document.getElementById('searchForm').addEventListener('submit', (e)=>{
-	search(e);
+document.getElementById('searchForm').addEventListener('submit', (e) => {
+    search(e);
 })
 
-function search(e){
-	e.preventDefault()
-fetch('/searchResults', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-            
-           "request": document.getElementById('search').value
-    })
-}).then(res=>res.json())
-  		.then(res => fun(res));
+function search(e) {
+    e.preventDefault()
+    fetch('/searchResults', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+
+            "request": document.getElementById('search').value
+        })
+    }).then(res => res.json())
+        .then(res => fun(res));
 }
-function fun(res){
-	console.log(res);
-	document.getElementById('results').innerHTML = "<h5>Results : </h5>" + res.result;
+
+function fun(res) {
+    console.log(res);
+    document.getElementById('results').innerHTML = "<h5>Results : </h5>" + res.result;
 }
